6c3717e8ef656991acd8e3329aafd937c7c1536e
Ospens <1ospens1@gmail.com>
2020-11-28 17:57:28 +0300

    Eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: ["@react-native-community", "airbnb", "airbnb/hooks", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "react/prop-types": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
