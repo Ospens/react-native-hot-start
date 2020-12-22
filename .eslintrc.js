2d79246de7e64f511fc0b2371d478842d8933fbf
Ospens <1ospens1@gmail.com>
2020-12-22 20:13:44 +0300

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
     "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
     "react/prop-types": "off",
     "import/extensions": [
